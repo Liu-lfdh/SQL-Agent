3bbe275cd99a5ca74a1e78fb205518b40fd11508
Liu MQ <1253976713@qq.com>
2026-04-12 16:07:59 +0800

    clear

--- Llm/Deepseek.py
+++ Llm/Deepseek.py
@@ -4,6 +4,6 @@ class Deepseek:
     @staticmethod
     def getLlm():
         return ChatOpenAI(
-            api_key="sk-ad872c810fdf4f2bb0dfb0e51115115f",
+            api_key="Your key",
             base_url="https://api.deepseek.com",
             model="deepseek-chat")
