24ffee81e01126eba9f44e7831ae018e52441c3d
Liu MQ <1253976713@qq.com>
2026-04-15 15:12:54 +0800

    env

--- Llm/Deepseek.py
+++ Llm/Deepseek.py
@@ -1,9 +1,13 @@
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Deepseek:
     @staticmethod
     def getLlm():
         return ChatOpenAI(
-            api_key="sk-ad872c810fdf4f2bb0dfb0e51115115f",
+            api_key=os.getenv("DEEPSEEK_API_KEY"),
             base_url="https://api.deepseek.com",
             model="deepseek-chat")
